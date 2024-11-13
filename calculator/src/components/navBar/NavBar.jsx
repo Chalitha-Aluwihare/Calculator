1186952e6401ab5752355a0dae32cc92cdcd0f3e
Chalitha Aluwihare <chalithabro1@gmail.com>
2024-11-13 18:28:45 +0530

    navBar comment update

--- calculator/src/components/navBar/NavBar.jsx
+++ calculator/src/components/navBar/NavBar.jsx
@@ -1,24 +1,32 @@
 import React from 'react'
-import Cal from './Calculator.png'
-import img02 from './img02.png'
-import img03 from './img03.png'
-import img04 from './img04.png'
+import Img01 from '../../assets/navBar/Calculator.png'
+import Img02 from '../../assets/navBar/img02.png'
+import Img03 from '../../assets/navBar/img03.png'
+import Img04 from '../../assets/navBar/img04.png'
 
 const NavBar = () => {
   return (
     <div className='flex items-center justify-center'>
     <div className='text-right w-[429px] bg-[#1E1E1E] p-2 flex'>
+      
+      {/*Calculator button */}
       <button className='mx-1 bg-[#0375F0] rounded-[100px] p-2'>
-            <img src={Cal} alt="" />
+            <img src={Img01} alt="Calculator" />
       </button>
+      
+      {/*Settings button */}
       <button className='mx-1 bg-[#ffffff] rounded-[100px] p-2'>
-            <img src={img02} alt="" />
+            <img src={Img02} alt="Settings" />
       </button>
+
+      {/*History button */}
       <button className='mx-1 bg-[#ffffff] rounded-[100px] p-2'>
-            <img src={img03} alt="" />
+            <img src={Img03} alt="History" />
       </button>
+      
+      {/*Home */}
       <button className='mx-1 bg-[#ffffff] rounded-[100px] p-2'>
-            <img src={img04} alt="" />
+            <img src={Img04} alt="Home" />
       </button>
     </div>
     </div>
