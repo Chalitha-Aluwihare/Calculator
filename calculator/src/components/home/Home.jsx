7f4b28e2324b2098db3ac83899d13d1c4d5c265d
Chalitha-Aluwihare <chalithaaluwihare@gmail.com>
2024-12-22 23:53:28 +0530

    Main Calculator Page

--- calculator/src/components/home/Home.jsx
+++ calculator/src/components/home/Home.jsx
@@ -1,26 +1,19 @@
-import React from 'react'
-import Img01 from '../../assets/home/img01.png'
+import React from "react";
+import Img01 from "../../assets/home/img01.png";
 
 const Home = () => {
   return (
-    <div className='relative flex flex-col items-center justify-center '>
-      <div className=''><img src={Img01} alt="" /></div>
-      <div className='grid grid-rows-9 '>
-        
-        <div className='inline-block row-span-2 bg-red-400'>WELCOME TO</div>
-        <div>AI-POWERED</div>
-        <div>CALCULATOR</div>
-
-        <div></div>
-
-        <div></div>
-        <div></div>
-        <div></div>
-
-      </div>   
+    <div className="">
+      {/* imge container */}
+      <div className="flex justify-center h-screen p-4 ">
+        <img className="rounded-xl" src={Img01} alt="" />
+        <div className="absolute bottom-5">
+        <span className="">Mian Calculator</span>
+        </div>
+      </div>
       
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
