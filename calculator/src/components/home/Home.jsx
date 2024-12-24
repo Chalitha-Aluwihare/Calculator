5511efdf4236dad81d21c8bf09b91d946d50517f
Chalitha-Aluwihare <chalithaaluwihare@gmail.com>
2024-12-24 22:14:05 +0530

    Home Page

--- calculator/src/components/home/Home.jsx
+++ calculator/src/components/home/Home.jsx
@@ -3,15 +3,62 @@ import Img01 from "../../assets/home/img01.png";
 
 const Home = () => {
   return (
-    <div className="">
-      {/* imge container */}
-      <div className="flex justify-center h-screen p-4 ">
-        <img className="rounded-xl" src={Img01} alt="" />
+    <div className="bg-[#5E5B5B]">
+      {/* Image container */}
+      <div className="relative flex justify-center h-screen p-4">
+        <img className="rounded-[30px]" src={Img01} alt="" />
+
         <div className="absolute bottom-5">
-        <span className="">Mian Calculator</span>
+          <button className="bg-[#0375F0] text-white px-5 py-2 rounded-full m-4">
+            Get Started
+          </button>
+          <div>
+            <h6 >WELCOME TO</h6>
+            <h6>
+              AI-POWERED
+            </h6>
+            <h6 >CALCULATOR</h6>
+            <div >
+              <h6 >
+                KEY FEATURES
+              </h6>
+              <h6 >
+                Voice-Powered Calculation
+              </h6>
+              <p >
+                Perform calculations by speaking, ideal for <br></br>
+                multitasking.Accurate, hands-free convenience.
+              </p>
+
+              <h6 >
+                Smart Photo Calculation
+              </h6>
+              <p >
+                Snap a photo of equations, and our app solves them, <br></br>
+                perfect for quick solutions from notes or books.
+              </p>
+
+              <h6 >
+                Precision Keyboard Input
+              </h6>
+              <p >
+                Traditional keyboard input with an intuitive interface <br></br>
+                for efficient calculations.
+              </p>
+
+              <h6 >
+                Streamlined User Interface
+              </h6>
+              <p >
+                User-friendly, clean design for easy access to all <br></br>
+                features, suitable for students and professionals.
+              </p>
+            </div>
+          </div>
         </div>
+
+        {/* Make this div a flex container */}
       </div>
-      
     </div>
   );
 };
